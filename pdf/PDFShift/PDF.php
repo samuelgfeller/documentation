3df9d111f5af252e5ed30794d01b7eb6a6bbba13
Samuel Gfeller <31797204+samuelgfeller@users.noreply.github.com>
2019-02-05 12:20:13 +0100

    Update PDF.php

--- pdf/PDFShift/PDF.php
+++ pdf/PDFShift/PDF.php
@@ -5,7 +5,7 @@ use \PDFShift\PDFShift;
 class PDF
 {
 	public function __construct() {
-		PDFShift::setApiKey('4f00ae5be5b34b5995ac110c719e6d7c');
+		PDFShift::setApiKey('apiKey');
 	}
 	
 	public function convert($data,$outputPath) {
